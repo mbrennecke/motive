8b5f424d3d53cf79b815f1a93e580e7414834314
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-01 21:09:00 -0600

    starting nav bar, making a mess

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink
+} from 'reactstrap';
+}
+import "./Nav.css";
+
+export default class MyNav extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+	render() {
+		return (
+			<Navbar color="light" light expand="md">
+				<NavbarBrand href="/">Motive Massage</NavbarBrand>
+		)
+	}
+}
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
       Motive Massage
     </a>
